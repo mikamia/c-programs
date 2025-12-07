54b46b26804e3d9d8f9bbb5b5a7c4cb6fe85835d
Ayako Yadav <ayakoyadav@Ayakos-MacBook-Air.local>
2025-12-06 21:22:30 -0500

    variables

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, C project!\\n");
+
+    int age = 20;
+
+    float height = 5.7;
+    char grade;
+
+    grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Grade: %c\n", grade);
+
+    printf("He:wqllo, C project!\\n");
     return 0;
 }
